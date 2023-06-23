d92b5c0c882f73b24e08839e2725ffc13370b558
HideOnBuche <129318425+HideOnBuche@users.noreply.github.com>
2023-06-23 18:55:16 +0000

    aqui me voy pa la casa

---
+++ ejmploreact/src/App.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+
+export function App(){
+    return(
+        <>
+            <h1>Hola mundo</h1>
+            <h1>lista de tareas</h1>
+            <ul class="list-group">
+            <li class="list-group-item list-group-item-action">tarea 1</li>
+            <li class="list-group-item list-group-item-action">tarea 2</li>
+            <li class="list-group-item list-group-item-action">tarea 3</li>
+            <li class="list-group-item list-group-item-action">tarea 4</li>
+            </ul>
+        </>
+    );
+}
