3d88610eb10c3d41d652423375cb1aae70c2a296
HideOnBuche <129318425+HideOnBuche@users.noreply.github.com>
2023-06-28 18:09:30 +0000

    creacion de modulos

--- ejmploreact/src/App.jsx
+++ ejmploreact/src/App.jsx
@@ -1,16 +1,11 @@
 import react from "react";
+import { TodoList } from "./componentes/TodoList";
 
 export function App(){
     return(
         <>
             <h1>Hola mundo</h1>
-            <h1>lista de tareas</h1>
-            <ul class="list-group">
-            <li class="list-group-item list-group-item-action">tarea 1</li>
-            <li class="list-group-item list-group-item-action">tarea 2</li>
-            <li class="list-group-item list-group-item-action">tarea 3</li>
-            <li class="list-group-item list-group-item-action">tarea 4</li>
-            </ul>
+            <TodoList></TodoList>
         </>
     );
 }
---
+++ ejmploreact/src/componentes/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function TodoItem(){
+    return(
+        <>
+            <li class="list-group-item list-group-item-action">tarea 1</li>
+        </>
+    )
+}
