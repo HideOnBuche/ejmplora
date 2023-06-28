aa81afbe2561c12ba9f8caefdee4cc2177c972ff
HideOnBuche <129318425+HideOnBuche@users.noreply.github.com>
2023-06-28 18:55:57 +0000

    editando el TodoList

--- ejmploreact/src/componentes/TodoList.jsx
+++ ejmploreact/src/componentes/TodoList.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoItem } from "./TodoItem";
+
+const KEY="TodoList-todos";
+
+export function TodoList(){
+    const [todos,sertodos,]=useState(
+        
+        JSON.parse(localStorage.getItem(KEY))?JSON.parse(localStorage.getItem(KEY)):[]            
+    )
+}
 
 export function TodoList(){
     return(
         <>
             <h1>lista de tareas</h1>
+            <div className="input-group mb-3 at-4">
+                <input ref={taskRef} placeholder="Ingrese una tarea" className="form-control" type="text" name="" id="" >                    
+                </input>
+                <button onClick={agregarTare} className="btn-success ms-2"><i className="bi bi-plus-circle-fill">
+                    </i></button>
+                <button onClick={eliminarTareCompletadas} className="btn btn-danger ms-2"><i className="bi bi-trash">
+                    </i></button>
+            </div>
             <ul class="list-group">
                 <TodoItem></TodoItem>
             </ul>
