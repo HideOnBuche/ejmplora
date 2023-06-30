abeecd76a8687cf7d82b163cd0b6a18f5cd1965e
HideOnBuche <129318425+HideOnBuche@users.noreply.github.com>
2023-06-30 18:03:52 +0000

    agregar tarea agregado

--- ejmploreact/src/componentes/TodoList.jsx
+++ ejmploreact/src/componentes/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react"
 import { TodoItem } from "./TodoItem";
 
 const KEY="TodoList-todos";
@@ -8,6 +8,32 @@ export function TodoList(){
         
         JSON.parse(localStorage.getItem(KEY))?JSON.parse(localStorage.getItem(KEY)):[]            
     )
+}
+
+const taskRef=useRef();
+
+useEffect(()=>{
+    localStorage.setItem(KEY,JSON.stringify(todos));
+    },[todos]);
+
+const agregarTarea=()=>{
+    const task=taskRef.current.value;
+    const id=uuid();
+    if(task=='')return;
+    setTodos((prevTodos)=>{
+        const newTask = {
+            id:id,
+            task:task,
+            complete:false
+        }
+        return[...prevTodos,newTask]
+
+    });
+    taskRef.current.value="";
+
+
+
+
 }
 
 export function TodoList(){
